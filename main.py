25ff088fc292bbd6af598f0b410913fdf2d89f9d
vietnh <vietnguyenhoang@vccorp.vn>
2023-01-05 14:30:41 +0700

    update python version

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ def index():
 
 if __name__ == "__main__":
     # Dev only: run "python main.py" and open http://localhost:8080
-    app.run(host="localhost", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
